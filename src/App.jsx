128fda060cbe93a3a1a7e4a79c628d9f1c41358a
Jaseem Quraishi <rdgvszasim@gmail.com>
2025-04-14 12:19:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { HomeScreen } from "./screens/HomeScreen";
+import { PlaygroundScreen } from "./screens/PlaygroundScreen";
+import { PlaygroundProvider } from "./Providers/PlaygroundProviders";
+import { ModalProvider } from "./Providers/ModalProvider";
+
+export default function App() {
+  return (
+    <>
+      <PlaygroundProvider>
+        <ModalProvider>
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route
+                path="/playground/:fileId/:folderId"
+                element={<PlaygroundScreen />}
+              />
+            </Routes>
+          </HashRouter>
+        </ModalProvider>
+      </PlaygroundProvider>
+    </>
+  );
+}
